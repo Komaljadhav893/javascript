812b1ea6a1e08d4e71db80356bf04c069a167f1b
Komal Jadhav <135141441+Komaljadhav893@users.noreply.github.com>
2024-05-07 16:56:01 +0530

    Add files via upload

---
+++ stack and heap.js
@@ -0,0 +1,17 @@
+// stack (primitive), heap(non-primitive)
+// let myYoutubeName="komaljadhav"
+// let anothername=myYoutubeName
+// console.log(anothername);
+// anothername="xyv";
+// console.log(anothername)
+
+// string
+// const name= "komal"
+// const repoCount=50
+// console.log(`hello my name is ${name} and myrepo count is ${repoCount}`);
+
+// const gameName= new String("HyeGoodMorningDude")
+// console.log(gameName.charAt(9));
+// const url=" komaljadhav45u698@3jvvhsj"
+// console.log(url.replace('@','-'))
+// strings method need to study more
