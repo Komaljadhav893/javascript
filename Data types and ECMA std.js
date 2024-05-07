345154f3c26e27c8ad213714233f78bb78ab2b46
Komal Jadhav <135141441+Komaljadhav893@users.noreply.github.com>
2024-05-07 10:59:35 +0530

    Add files via upload

---
+++ Data types and ECMA std.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as never version
+// alert(3+3)
+//  we are not using brower here 
+/* Notes
+ The alert dialog should be used for messages which do not require any response on the part of the user, other than the acknowledgement of the message.
+
+ Dialog boxes are modal windows - they prevent the user from accessing the rest of the program's interface until the dialog box is closed. For this reason, you should not overuse any function that creates a dialog box (or modal window). */
+
+
+
+ // number => 2 to the power 78
+ // bigint
+ // string => " "
+ // boolean => true / false
+ //undefined 
+ // unll
+ // symbol
+ // object
+console.log(typeof(undefined)); // undefined
+console.log(typeof(null) ) // object
+
+
+
+
+   
