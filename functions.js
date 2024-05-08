73457e9a3aea53fff51ca34868eed3cf296ad8fa
Komal Jadhav <135141441+Komaljadhav893@users.noreply.github.com>
2024-05-08 16:24:12 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,80 @@
+// console.log("k")
+// console.log("k")
+// console.log("k")
+// console.log("k")
+// function sayName () {
+//   console.log("k");
+//   console.log("o");
+//   console.log("m");
+//   console.log("a")
+//   console.log("l")
+// }
+// sayName()
+// sayName()
+// sayName()
+
+
+
+
+// function addTwoNums(num1 , num2) {
+//   console.log(num1 + num2)
+// }
+// addTwoNums(4,9) // 13
+
+
+
+
+
+// function addTwoNums(num1 , num2) {
+//   console.log(num1 + num2)
+// }
+// addTwoNums(4,'9') // 49
+
+
+
+
+
+// function addTwoNums(num1 , num2) {
+//   console.log(num1 + num2)
+// }
+// addTwoNums(4, null) // 4
+
+
+
+
+// function addTwoNums(num1 , num2) {
+//   console.log(num1 + num2)
+// }
+// const result =addTwoNums(4, 9) 
+// console.log("result :",result)  //13 result : undefined
+
+
+
+
+
+
+//  function addTwoNums(num1 , num2) {
+//   //  console.log(num1 + num2)
+//   return num1 + num2
+//  }
+// const result =addTwoNums(4, 9) 
+// console.log("result :",result) // result : 13
+
+
+
+
+// function loginUser(username){
+//   if(!username){
+//     console.log("please eter your user name :")
+//   }
+//   return `${username} just logged in `
+// }
+// console.log(loginUser("komal")) // komal just logged in
+
+
+
+
+function calculateCardPrice(val1, val2, ...num1){
+  return num1
+}
+console.log(calculateCardPrice(2,90,45,234))
