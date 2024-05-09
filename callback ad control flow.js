50d9cb58d9386fd6459f05f97c92063f719d9cb8
Komal Jadhav <135141441+Komaljadhav893@users.noreply.github.com>
2024-05-09 12:13:29 +0530

    Add files via upload

---
+++ callback ad control flow.js
@@ -0,0 +1,25 @@
+// global execution context
+// function execution context
+// eval execution context
+// { } => memory creation phase
+//  => execution phase
+// if 
+// const isUserLoggin=true
+// if(2=='2')
+//   console.log("executed");{
+   
+// }
+// <,>,<=,>=
+// switch case
+// const key=0;
+// switch (key){
+//   case value:
+//     break;
+
+
+
+//     default:
+//       break;
+
+// }
+
