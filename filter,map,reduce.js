17389a3beb53c06cce9d22a6b9ee33db0a426884
Komal Jadhav <135141441+Komaljadhav893@users.noreply.github.com>
2024-05-10 09:41:25 +0530

    Add files via upload

---
+++ filter,map,reduce.js
@@ -0,0 +1,11 @@
+// const items=["komal","kiru","uttam"]
+// const values=items.forEach((item)=>{
+//   console.log(item)
+// })
+// console.log(values)
+
+const myNums= [1,2,3,4,5,6,7,8,9]
+const newNums=myNums.filter((num)=>{
+  return num>5
+})
+console.log(newNums)
