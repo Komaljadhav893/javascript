255e022306c58ea42dd9c51285d4263b2059fa75
Komal Jadhav <135141441+Komaljadhav893@users.noreply.github.com>
2024-05-13 19:24:10 +0530

    Add files via upload

---
+++ oops.js
@@ -0,0 +1,41 @@
+//prototype based language
+// oops => programing paraadyme 
+
+// object=> collection of propertiees and method 
+// constructor function ,propertiesfunctions
+
+// const user={
+//   username:"komal",
+//   loginount:4,
+//   signedIn:true,
+
+//   getUserDetails:function(){
+//     console.log("get user details from database");
+//   }
+// }
+// console.log(user.username)
+function User(username,loginount,signedIn){
+  this.username=username;
+  this.loginount=loginount
+  this.signedIn=signedIn
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
